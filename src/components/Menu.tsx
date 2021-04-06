3f18424f6b2d8ed87514d4805b001c884e6faa94
Wes PC <wesleyoliveirap@gmail.com>
2021-04-06 00:53:19 -0300

    NavLink

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,29 +1,30 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Menu = () => {
-    return (
-        <nav id="sidebarMenu" className="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
-        <div className="position-sticky pt-3">
-          <ul className="nav flex-column">
-            <li className="nav-item">
-              <Link to={'/'} className="nav-link active">
-                <span data-feather="home"></span>
-                Dashboard
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to={'/users'} className="nav-link active">
-                <span data-feather="home"></span>
-                Users
-              </Link>
-            </li>
-          </ul>
-  
-  
-        </div>
-      </nav>
-    );
-}
+  return (
+    <nav
+      id="sidebarMenu"
+      className="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse"
+    >
+      <div className="position-sticky pt-3">
+        <ul className="nav flex-column">
+          <li className="nav-item">
+            <NavLink to={"/"} exact className="nav-link">
+              <span data-feather="home"></span>
+              Dashboard
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink to={"/users"} className="nav-link">
+              <span data-feather="home"></span>
+              Users
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
 export default Menu;
